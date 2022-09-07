7fa187bfcc561cbaebc18c3de03abede95050472
Dilzpx01 <107130420+dilzpx@users.noreply.github.com>
2022-09-07 15:29:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /** 
-    - Created By : Riy 04
-    - Base Ori : Irfan/Rtwone
+    - Created By : DILZPX
+    - Base Ori : PENDOLEXE
     - Mau yang no Enc? Buy bang
     - Minat? Sung chat : https://wa.me/6281575886399
 */
